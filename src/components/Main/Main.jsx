a6a4deaa6b65ccada99b2170b8917f0993631c2a
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2022-01-22 22:19:47 +0500

    feat: header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,18 @@
 import './Main.css';
+import Header from '../Header/Header';
+
 
 function Main() {
     return (
         <>
+            <Header />
             <main className="main">
                 <p>
                     Отображается страница «О проекте»
                 </p>
             </main>
         </>
+
     )
 }
 
