d0684e83db753bd77d8872fb6192a2755e4ba1be
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2022-01-28 09:10:01 +0500

    feat: сверстать компонент Portfolio

--- src/components/Main/Portfolio.jsx
+++ src/components/Main/Portfolio.jsx
@@ -3,23 +3,21 @@ import arrow from '../../images/text__COLOR_font-main.svg';
 
 function Portfolio() {
     return (
-        <>
-            <div className="portfolio">
-                <h3 className="portfolio__title">Портфолио</h3>
-                <a className="portfolio__link" href='#'>
-                    <p className="portfolio__link-text">Статичный сайт</p>
-                    <img className="portfolio__link-img" src={arrow} alt="стрелка" />
-                </a>
-                <a className="portfolio__link" href='#'>
-                    <p className="portfolio__link-text">Адаптивный сайт</p>
-                    <img className="portfolio__link-img" src={arrow} alt="стрелка" />
-                </a>
-                <a className="portfolio__link" href='#'>
-                    <p className="portfolio__link-text">Одностраничное приложение</p>
-                    <img className="portfolio__link-img" src={arrow} alt="стрелка" />
-                </a>
-            </div>
-        </>
+        <div className="portfolio">
+            <h3 className="portfolio__title">Портфолио</h3>
+            <a className="portfolio__link" href='#'>
+                <p className="portfolio__link-text">Статичный сайт</p>
+                <img className="portfolio__link-img" src={arrow} alt="стрелка" />
+            </a>
+            <a className="portfolio__link" href='#'>
+                <p className="portfolio__link-text">Адаптивный сайт</p>
+                <img className="portfolio__link-img" src={arrow} alt="стрелка" />
+            </a>
+            <a className="portfolio__link" href='#'>
+                <p className="portfolio__link-text">Одностраничное приложение</p>
+                <img className="portfolio__link-img" src={arrow} alt="стрелка" />
+            </a>
+        </div>
     )
 }
 
