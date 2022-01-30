a32444d2fee5895cbbe2bd729424570068605adc
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2022-01-30 15:57:49 +0500

    feat: адаптировать компонент Moveis

--- src/components/Main/Portfolio.jsx
+++ src/components/Main/Portfolio.jsx
@@ -3,7 +3,7 @@ import arrow from '../../images/text__COLOR_font-main.svg';
 
 function Portfolio() {
     return (
-        <div className="portfolio">
+        <section className="portfolio">
             <h3 className="portfolio__title">Портфолио</h3>
             <a className="portfolio__link" href='#'>
                 <p className="portfolio__link-text">Статичный сайт</p>
@@ -17,7 +17,7 @@ function Portfolio() {
                 <p className="portfolio__link-text">Одностраничное приложение</p>
                 <img className="portfolio__link-img" src={arrow} alt="стрелка" />
             </a>
-        </div>
+        </section>
     )
 }
 
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,6 +2,7 @@ import Header from "../Header/Header";
 import SearhForm from "./SearchForm";
 import Footer from "../Footer/Footer";
 import MoviesCardList from "./MoviesCardList";
+import Preloader from "./Preloader";
 import './Movies.css';
 
 function Movies(props) {
@@ -13,6 +14,7 @@ function Movies(props) {
             <main className="movies">
                 <SearhForm />
                 <MoviesCardList />
+                <Preloader />
             </main>            
             <Footer />
         </>
