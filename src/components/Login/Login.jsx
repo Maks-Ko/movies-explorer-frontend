498439b0b055aea25d8feaf1de683560e5cde6a0
Maksim Kozhevnikov <kozhevnikow@yandex.ru>
2022-01-30 21:30:59 +0500

    feat: сверстать компоненты Login и Register

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,11 +1,21 @@
+import PageWithForm from "../PageWithForm/PageWithForm";
+
 function Login() {
     return (
         <>
-            <main className="main">
-                <p>
-                    Отображается страница авторизации
-                </p>
-            </main>
+            <PageWithForm
+                title="Рады видеть!"
+                textButton="Войти"
+                text="Ещё не зарегистрированы?"
+                link="/signup"
+                linkText="Регистрация"
+            >
+                <span for="user_name" className="form__text-span">Имя</span>
+                <input id="user_name" name="user_name" type="text" className="form__text-input" />
+
+                <span for="password" className="form__text-span">Пароль</span>
+                <input id="password" name="password" type="password" className="form__text-input" />
+            </PageWithForm>
         </>
     )
 }
